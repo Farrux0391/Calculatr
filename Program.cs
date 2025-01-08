7c51b4244e11a4d95af138125be2f7b874b12bd0
Farrux0391 <farrux0391@gmail.com>
2025-01-08 21:10:32 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber =Convert.ToInt32(Console.ReadLine());
+
+//increment + 1
+System.Console.WriteLine(firstNumber++);
+System.Console.WriteLine(firstNumber);
